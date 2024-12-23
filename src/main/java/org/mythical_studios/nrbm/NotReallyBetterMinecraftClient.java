b462e8da0b118523ad8f530b16b60e0136e4322c
MarsMyth <callumshelton2503@gmail.com>
2024-12-23 11:14:58 +1000

    Initial Commit

---
+++ src/main/java/org/mythical_studios/nrbm/NotReallyBetterMinecraftClient.java
@@ -0,0 +1,10 @@
+package org.mythical_studios.nrbm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NotReallyBetterMinecraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/org/mythical_studios/nrbm/NotReallyBetterMinecraftDataGenerator.java
@@ -0,0 +1,11 @@
+package org.mythical_studios.nrbm;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NotReallyBetterMinecraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
